936958d2bba5bbbb355b7a86820fbca9a4e59276
stevens1434 <stevens1434@gmail.com>
2017-11-20 14:22:43 -0800

    heroku things

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 app.use(function(req, res, next) {
   // before every route, attach the flash messages and current user to res.locals
